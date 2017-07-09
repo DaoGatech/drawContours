d3a6e31a69cb62740f22afae1f697adff42d8cf6
DaoGatech <stephenhdao@gatech.edu>
2017-07-09 17:01:41 -0500

    Simplify logic

--- drawContours.py
+++ drawContours.py
@@ -36,62 +36,21 @@ def analyze():
 
     for cnt in contours:
         # Get approximate polygons
-        approx = cv2.approxPolyDP(cnt, 0.05 * cv2.arcLength(cnt, True), True)
+        approx = cv2.approxPolyDP(cnt, 0.03 * cv2.arcLength(cnt, True), True)
 
         print(len(approx))
         if len(approx) == 3:
-            shape_name = "Triangle"
-            cv2.drawContours(image, [cnt], 0, (0, 255, 0), -1)
-            print("TRIANGLE")
-            # Find contour center to place text at the center
-            M = cv2.moments(cnt)
-            cx = int(M['m10'] / M['m00'])
-            cy = int(M['m01'] / M['m00'])
-            cv2.putText(image, shape_name, (cx - 50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+            return "Triangle"
 
         elif len(approx) == 4:
             x, y, w, h = cv2.boundingRect(cnt)
-            M = cv2.moments(cnt)
-            cx = int(M['m10'] / M['m00'])
-            cy = int(M['m01'] / M['m00'])
-
-            # Check to see if 4-side polygon is square or rectangle
-            # cv2.boundingRect returns the top left and then width and
             if abs(w - h) <= 3:
-                shape_name = "Square"
-
-                # Find contour center to place text at the center
-                cv2.drawContours(image, [cnt], 0, (0, 125, 255), -1)
-                cv2.putText(image, shape_name, (cx - 50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+                return "Square"
             else:
-                shape_name = "Rectangle"
-
-                # Find contour center to place text at the center
-                cv2.drawContours(image, [cnt], 0, (0, 0, 255), -1)
-                M = cv2.moments(cnt)
-                cx = int(M['m10'] / M['m00'])
-                cy = int(M['m01'] / M['m00'])
-                cv2.putText(image, shape_name, (cx - 50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
-
+                return "Rectangle"
         elif len(approx) == 10:
-            shape_name = "Star"
-            cv2.drawContours(image, [cnt], 0, (255, 255, 0), -1)
-            M = cv2.moments(cnt)
-            cx = int(M['m10'] / M['m00'])
-            cy = int(M['m01'] / M['m00'])
-            cv2.putText(image, shape_name, (cx - 50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+            return "Star"
 
-
-
-        elif len(approx) >= 15:
-            shape_name = "Circle"
-            cv2.drawContours(image, [cnt], 0, (0, 255, 255), -1)
-            M = cv2.moments(cnt)
-            cx = int(M['m10'] / M['m00'])
-            cy = int(M['m01'] / M['m00'])
-            cv2.putText(image, shape_name, (cx - 50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
-
-        cv2.waitKey(0)
 
 if __name__ == '__main__':
     app.run()
